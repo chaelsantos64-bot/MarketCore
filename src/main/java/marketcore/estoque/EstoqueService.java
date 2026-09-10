770d7e6c140a71f855c5a9aac4040ae13dc8a358
Michael Costa <chaelsantos64@gmail.com>
2026-09-10 10:36:23 -0300

    feat: atualiza funcionalidades de estoque

--- src/main/java/marketcore/estoque/EstoqueService.java
+++ src/main/java/marketcore/estoque/EstoqueService.java
@@ -1,8 +1,11 @@
 package marketcore.estoque;
 
 
+import marketcore.exception.CancelamentoPedidoException;
+import marketcore.exception.EstoqueInsuficienteException;
+
 public class EstoqueService {
-    public Boolean verificarDisponibilidade(
+    public boolean verificarDisponibilidade(
             Estoque estoque, int quantidDesejada){
         if (estoque.getQuantidadeDisponivel() >= quantidDesejada) {
             return true;
@@ -10,12 +13,43 @@ public class EstoqueService {
         return false;
     }
 
-    public void reduzirEstoque(Estoque estoque, int quantidade){
+    public void reduzirEstoque(Estoque estoque, int quantidade) {
+        if (quantidade <= 0) {
+            throw new IllegalArgumentException(
+                    "Quantidade deve ser maior que zero"
+            );
+        }
+        if (estoque.getQuantidadeDisponivel() < quantidade) {
+            throw new EstoqueInsuficienteException(
+                    "Estoque insuficiente"
+            );
+        }
+
         int novaQuantidade =
                 estoque.getQuantidadeDisponivel() - quantidade;
 
-        if (novaQuantidade >= quantidade) {
+        estoque.getProduto().setQuantidade(novaQuantidade);
+    }
 
+    public void adicionarEstoque(Estoque estoque, int quantidade){
+        if (quantidade <= 0) {
+            throw new EstoqueInsuficienteException(
+                    "Estoque insuficiente"
+            );
         }
+
+        int novaQuantidade = estoque.getQuantidadeDisponivel() + quantidade;
+        estoque.getProduto().setQuantidade(novaQuantidade);
+
+    }
+    public void devolverEstoque(Estoque estoque, int quantidade){
+        if (quantidade <= 0) {
+            throw new EstoqueInsuficienteException(
+                    "Estoque insuficiente"
+            );
+        }
+        int qDevolvida = estoque.getQuantidadeDisponivel() + quantidade;
+        estoque.getProduto().setQuantidade(qDevolvida);
+
     }
 }
